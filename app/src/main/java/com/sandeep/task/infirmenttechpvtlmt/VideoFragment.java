aa84525215abf5023aee68fdd77d47e070111c3d
sd@sd.com <sd@sd.com>
2020-10-07 09:54:58 +0530

    loginSignResetForgot

---
+++ app/src/main/java/com/sandeep/task/infirmenttechpvtlmt/VideoFragment.java
@@ -0,0 +1,17 @@
+package com.sandeep.task.infirmenttechpvtlmt;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class VideoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+}
